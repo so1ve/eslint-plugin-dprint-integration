fd70af5c1d50f47e57fad1cdb0e257fce95d2649
so1ve <i@mk1.io>
2023-12-22 08:52:43 +0800

    test: move to vite-native glob

--- test/format.test.ts
+++ test/format.test.ts
@@ -1,16 +1,8 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
-
 import { Linter } from "eslint";
 import * as parserPlain from "eslint-parser-plain";
-import fg from "fast-glob";
 import { describe, expect, it } from "vitest";
 
 import DprintIntegration from "../src";
-import { dir } from "./utils";
-
-const dirname = dir();
-const relative = (path: string) => join(dirname, path);
 
 const linter = new Linter();
 const linterConfig = {
@@ -23,20 +15,13 @@ const linterConfig = {
 linter.defineParser("plain", parserPlain as any);
 linter.defineRule("dprint", DprintIntegration.rules.dprint);
 
-async function runFixtures(fixtures: string) {
-	const fixtureFileList = await fg(fixtures.replaceAll("\\", "/"));
-	const fixtureFiles = await Promise.all(
-		fixtureFileList.map(async (file) => {
-			const source = await readFile(file, { encoding: "utf-8" });
+describe("format", async () => {
+	const fixtureFiles = import.meta.glob("./__fixtures__/format/**", {
+		eager: true,
+		as: "raw",
+	});
 
-			return {
-				file,
-				source,
-			};
-		}),
-	);
-
-	const results = fixtureFiles.map(({ file, source }) => {
+	const results = Object.entries(fixtureFiles).map(([file, source]) => {
 		const { output } = linter.verifyAndFix(source, linterConfig, file);
 
 		return {
@@ -44,12 +29,6 @@ async function runFixtures(fixtures: string) {
 			output,
 		};
 	});
-
-	return results;
-}
-
-describe("format", async () => {
-	const results = await runFixtures(relative("./__fixtures__/format/**"));
 
 	for (const { file, output } of results) {
 		it(`File: ${file}`, async () => {
